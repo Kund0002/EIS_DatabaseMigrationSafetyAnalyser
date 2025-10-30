0979eb503d2725ef47ed96dfdbb2aa21f17aaadb
Adrien-Hauskov <hauskovpedersen@gmail.com>
2025-10-30 14:40:53 +0100

    initial parser implementation

---
+++ src/migration_analyser/parser.py
@@ -0,0 +1,14 @@
+from pathlib import Path
+
+import sqlglot
+
+sql_file = "examplesql.sql"
+sql_text = Path(sql_file).read_text()
+
+sql = sqlglot.parse(sql_text)
+
+for _i, stmt in enumerate(sql, start=1):
+    print("\nStatement")
+    print(stmt)
+    print("\nAST")
+    print(stmt.dump())
